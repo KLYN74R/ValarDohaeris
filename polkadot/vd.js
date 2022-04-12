764a49111f0051910861a6d029c8beaa888a8d63
Vlad Chernenko <vladchernenko@protonmail.com>
2022-04-13 02:10:21 +0300

    Removed nacl lib in Polkadot.Now under the hood we have native ed22519

--- polkadot/vd.js
+++ polkadot/vd.js
@@ -1,57 +1,63 @@
-import{encodeAddress,mnemonicGenerate,mnemonicToMiniSecret} from '@polkadot/util-crypto'
-
-import nacl from 'tweetnacl'
+import{encodeAddress} from '@polkadot/util-crypto'
 
 
 //Ed25519,ECDSA(secp256k1) and other (doesn't matter here)
-export default {
+export default{
 
-
-    generate:(mnemonic=mnemonicGenerate())=>{
-
-        let seed=mnemonicToMiniSecret(mnemonic),
-
-            {publicKey,secretKey} = nacl.sign.keyPair.fromSeed(seed)
-
-        return {
-            
-            seed:Buffer.from(seed).toString('base64'),
- 
-            publicKey:Buffer.from(publicKey).toString('base64'),
- 
-            secretKey:Buffer.from(secretKey).toString('base64'),
- 
-            address:encodeAddress(publicKey,0)
+    generate:()=>new Promise((resolve,reject)=>{
         
-        }
+        crypto.generateKeyPair('ed25519',{
+            
+            publicKeyEncoding: {type:'spki',format:'der'},
+            privateKeyEncoding: {type:'pkcs8',format:'der'}
+         
+        },(err,publicKey,privateKey)=>{
+    
+            if(err) reject(false)
+            
+            else {
+    
+                publicKey=new Uint8Array(publicKey.slice(12))//if hex(Common-302a300506032b6570032100) slice(16) if base64(Common-MCowBQYDK2VwAyEA)  GfHq2tTVk9z4eXgy-for BASE58
+    
+                privateKey=privateKey.toString('hex')       
+    
+                resolve({polkaAddress:encodeAddress(publicKey),privateKey})
+    
+            }
+    
+        })
+    
+    }).catch(e=>false),
 
-    },
 
-
-    sign:(data,privateKey)=>
+    sign:(data,hexPrivateKey)=>new Promise((resolve,reject)=>
         
-        //Return signature in base64 format for easy transfer
-        Buffer.from(nacl.sign.detached( new Uint8Array(data),  new Uint8Array(Buffer.from(privateKey,'base64')))).toString('base64'),
-
-
-
-    verify:(data,signature,pubKey)=>
+        crypto.sign(null,Buffer.from(data),'-----BEGIN PRIVATE KEY-----\n'+Buffer.from(hexPrivateKey,'hex').toString('base64')+'\n-----END PRIVATE KEY-----',(e,sig)=>
     
-        nacl.sign.detached.verify(
-            
-            new Uint8Array(data),
-            new Uint8Array(Buffer.from(signature,'base64')),
-            new Uint8Array(Buffer.from(pubKey,'base64'))
-            
-        ),
+            e?reject(''):resolve(sig.toString('base64'))
 
+        )
 
+    ).catch(e=>''),
+
+       
+
+    verify:(data,signature,polkaAddress)=>new Promise((resolve,reject)=>
+       
+        //Add mandatory prefix and postfix to pubkey
+        crypto.verify(null,data,'-----BEGIN PUBLIC KEY-----\n'+Buffer.concat([Buffer.from('302a300506032b6570032100','hex'),decodeAddress(polkaAddress,0)]).toString('base64')+'\n-----END PUBLIC KEY-----',Buffer.from(signature,'base64'),(err,res)=>
+
+            err?reject(false):resolve(res)
+
+        )
     
+    ).catch(e=>false),
+
+
     toKusama:pubKey=>encodeAddress(Buffer.from(pubKey,'base64'),2),
 
     toSubstrate:pubKey=>encodeAddress(Buffer.from(pubKey,'base64'),42),
 
     toPolkadot:pubKey=>encodeAddress(Buffer.from(pubKey,'base64'),0),
-
 
 }
