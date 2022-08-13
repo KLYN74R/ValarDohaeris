86c8a818a8b9aa2e067938fe672b6019374f6ee0
Vlad Chernenko <vladchernenko@protonmail.com>
2022-08-14 00:20:13 +0300

    Fixes in TON. Now TON is ready to VD

--- ton/vd.js
+++ ton/vd.js
@@ -1,80 +1,58 @@
-import crypto from 'crypto'
+import nacl from 'tweetnacl'
 import tonweb from 'tonweb'
+import bip39 from 'bip39'
 
 
 
 
-//KLYNTAR native format
-//RFC8410 Ed25519 keypair with base58 encoded pubkey as address and 64 bytes base64 encoded signature
-export let DA = {
+export default {
 
 
-    generate:()=>new Promise((resolve,reject)=>{
+    generate:async(mnemonic,mnemoPassword)=>{
+
+        mnemonic ||=bip39.generateMnemonic()
+
+        let seed = bip39.mnemonicToSeedSync(mnemonic,mnemoPassword)
         
-        crypto.generateKeyPair('ed25519',{
+        let pair = nacl.sign.keyPair.fromSeed(seed.slice(0,32))
+
+     
+        return {
             
-            publicKeyEncoding: {type:'spki',format:'der'},
-            privateKeyEncoding: {type:'pkcs8',format:'der'}
-         
-        },async(err,publicKey,privateKey)=>{
+            privateKey: Buffer.from(pair.secretKey).toString('hex'),
 
-            if(err) reject(falses)
+            pubkey:Buffer.from(pair.publicKey).toString('hex'),
 
-            else{
+            address:(await new tonweb().wallet.create({publicKey:pair.publicKey}).getAddress()).toString(true, true, true, false),
 
-                publicKey=publicKey.slice(12)
-
-                privateKey=Buffer.concat([privateKey.slice(16),publicKey]).toString('hex')
-                
-                resolve(
-                    
-                    {
-                        publicKey:publicKey.toString('hex'),
-                        
-                        privateKey,
-                        
-                        address:(await new tonweb().wallet.create({publicKey}).getAddress()).toString(true, true, true, false)
-                    
-                    }
-                    
-                )
-
-            }
-
-        })
-      
-    }).catch(e=>false),
-
-
-
-    sign:(data,privateKey)=>new Promise((resolve,reject)=>
+            mnemonic
         
-        crypto.sign(null,Buffer.from(data),'-----BEGIN PRIVATE KEY-----\n'+privateKey+'\n-----END PRIVATE KEY-----',(e,sig)=>
+        }
     
-            e?reject(''):resolve(sig.toString('base64'))
+    },
 
+
+    sign:(data,privateKey)=>Buffer.from(
+        
+        nacl.sign.detached(
+            
+            new Uint8Array(Buffer.from(data,'utf-8')),
+            new Uint8Array(Buffer.from(privateKey,'hex'))
+            
         )
+        
+    ).toString('base64'),
 
-    ).catch(e=>''),
 
-       
-
-    verify:(data,signature,pubKey)=>new Promise((resolve,reject)=>
-       
-        //Add mandatory prefix and postfix to pubkey
-        crypto.verify(null,data,'-----BEGIN PUBLIC KEY-----\n'+Buffer.from(Base58.decode('GfHq2tTVk9z4eXgy'+pubKey)).toString('base64')+'\n-----END PUBLIC KEY-----',Buffer.from(signature,'base64'),(err,res)=>
-
-            err?reject(false):resolve(res)
-
-        )
-
-    ).catch(e=>false)
+    verify:(data,signature,pubKey)=>
     
+        nacl.sign.detached.verify(
+            
+            new Uint8Array(Buffer.from(data,'utf-8')),
+            new Uint8Array(Buffer.from(signature,'base64')),
+            new Uint8Array(Buffer.from(pubKey,'hex'))
+            
+        )
+    ,
 
 }
-
-let kp=await DA.generate()
-
-console.log(kp)
-
-console.log((await new tonweb().wallet.create({publicKey:Buffer.from(kp.publicKey,'hex')}).getAddress()).toString(true, true, true, false))
